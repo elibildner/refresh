95512621facdf19edd173fbdc6f552d53d902b1d
Eli Bildner <elias.bildner@gmail.com>
2013-08-15 23:14:32 -0700

    added friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
+
   def new
   	@user = User.new
   end
@@ -18,11 +22,35 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Your settings have been updated."
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+      flash[:error] = "Your settings were not updated successfully."
+    end
+  end
+
   private
 
 	def user_params
 		params.require(:user).permit(:name, :email, :password,
 	                               :password_confirmation)
 	end
+
+  def signed_in_user
+    store_location
+    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url, notice: "You do not have permission to edit this page." unless @user == current_user 
+  end
 
 end
