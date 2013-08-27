e9f91034ceda3436b0372815d2d92fc324911da6
Eli Bildner <elias.bildner@gmail.com>
2013-08-26 22:17:57 -0700

    finished user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
 
   def new
@@ -9,6 +10,10 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def create
@@ -36,6 +41,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+
   private
 
 	def user_params
@@ -51,6 +62,10 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to root_url, notice: "You do not have permission to edit this page." unless @user == current_user 
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 
 end
